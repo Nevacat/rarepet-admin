02f771aaf0690aef424cd68236f01d814b256c1a
Nevacat <nevacattery217@naver.com>
2023-11-29 10:47:06 +0900

    feat:공통 Header 구현

---
+++ src/components/Common/Headers/index.tsx
@@ -0,0 +1,13 @@
+import * as S from './styles';
+function Headers() {
+  return (
+    <S.HeaderWrap>
+      <S.LogoWrap>
+        <S.Box />
+        <S.LogoText>RAREPET</S.LogoText>
+      </S.LogoWrap>
+    </S.HeaderWrap>
+  );
+}
+
+export default Headers;
---
+++ src/components/Common/Headers/styles.ts
@@ -0,0 +1,30 @@
+import styled from '@emotion/styled';
+
+export const HeaderWrap = styled.div`
+  width: 100vw;
+  height: 60px;
+  background-color: #fff;
+  border-bottom: 1px solid #ddd;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+`;
+export const LogoWrap = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Box = styled.div`
+  width: 2rem;
+  height: 2rem;
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.colors.brown};
+`;
+
+export const LogoText = styled.h1`
+  color: ${({ theme }) => theme.colors.brown};
+  font-size: 1.6rem;
+  font-weight: 700;
+  margin-left: 5px;
+`;
